2775cd97fec938efdac72d988528ed56e73eab94
SURYANSHU KUMAR RAI <117182159+suryanshukumarrai@users.noreply.github.com>
2025-08-13 10:10:12 +0530

    Time: 3 ms (60.14%) | Memory: 9 MB (14.61%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if(n<=0)return false;
-        while(n%3==0){
-            n/=3;
+        // while(n%3==0){
+        //     n/=3;
 
-        }
-        return n==1;
+        // }
+        // return n==1;
+        double logValue = log10(n) / log10(3);
+
+    // Check if logValue is close to an integer
+    return fabs(logValue - round(logValue)) < 1e-10;
     }
 };
