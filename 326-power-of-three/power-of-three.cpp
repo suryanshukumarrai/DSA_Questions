aecf2244f3ce73e03ada4a1795ce63edeea72833
SURYANSHU KUMAR RAI <117182159+suryanshukumarrai@users.noreply.github.com>
2025-08-13 10:11:00 +0530

    Time: 14 ms (5.68%) | Memory: 9.1 MB (5.34%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -7,9 +7,15 @@ public:
 
         // }
         // return n==1;
-        double logValue = log10(n) / log10(3);
+    //     double logValue = log10(n) / log10(3);
 
-    // Check if logValue is close to an integer
-    return fabs(logValue - round(logValue)) < 1e-10;
+    // // Check if logValue is close to an integer
+    // return fabs(logValue - round(logValue)) < 1e-10;
+    int num = 0;
+        for(int i = 0; pow(3,i) <= n; i++){
+            num = pow(3,i);
+            if(num == n) return true;
+        }
+        return false;
     }
 };
