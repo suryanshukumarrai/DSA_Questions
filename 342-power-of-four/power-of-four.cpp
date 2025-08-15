e1b939d7b262ce989e0805bd2959a578832a88b8
SURYANSHU KUMAR RAI <117182159+suryanshukumarrai@users.noreply.github.com>
2025-08-15 08:57:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (18.14%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==0)return false;
-        while(n%4==0){
-            n/=4;
+        for(long long i=1;i<=n;i*=4){
+            if(i==n){
+                return true;
+            }
         }
-        return n==1;
+        return false;
         
     }
 };
