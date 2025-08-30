89355974aac48baa9021cf84e681458465aa1266
SURYANSHU KUMAR RAI <117182159+suryanshukumarrai@users.noreply.github.com>
2025-08-31 00:08:37 +0530

    Time: 3 ms (66.41%) | Memory: 8.3 MB (11.61%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    //int abb(int num, vector<int>& arr) {
-        // if (num == 0 || num == 1) {
-        //     return num;
-        // }
-
-        // if (arr[num] != -1) {
-        //     return arr[num];
-        // }
-
-        // return arr[num] = fib(num - 1) + fib(num - 2);
-        
-    //}
     int fib(int num) {
 
         vector<int> arr(num + 1,0);
