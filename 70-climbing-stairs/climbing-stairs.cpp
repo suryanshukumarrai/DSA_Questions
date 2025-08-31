be256f69b553a1621084c0793cff82382aa3f51c
SURYANSHU KUMAR RAI <117182159+suryanshukumarrai@users.noreply.github.com>
2025-08-31 12:36:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.17%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -6,11 +6,12 @@ public:
         }
         int a=1;
         int b=1;
+        vector<int>arr(n+1,-1);
+        arr[0]=1;
+        arr[1]=1;
         for(int i=2;i<=n;i++){
-            int cur=a+b;
-            a=b;
-            b=cur;
+            arr[i]=arr[i-1]+arr[i-2];
         }
-        return b;
+        return arr[n];
     }
 };
