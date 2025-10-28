36b70d453d9ae88da25943fd444ac3c8253c1275
SURYANSHU KUMAR RAI <117182159+suryanshukumarrai@users.noreply.github.com>
2025-10-28 13:23:26 +0530

    Time: 8 ms (40.83%) | Memory: 23 MB (28.90%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
-        int total = accumulate(nums.begin(), nums.end(), 0LL), ls = 0, cases = 0;
-        for (int num : nums) {
-            int rs = total - ls - num;
-            if (num == 0) {
-                if (ls == rs) {
-                    cases += 2;
-                } else if (ls == rs - 1 || ls - 1 == rs) {
-                    cases++;
-                }
-            }
-            ls += num;
-        }
-        return cases;
+        int n = nums.size();
+    vector<long long> prefix(n + 1, 0);
+
+    for (int i = 0; i < n; i++) {
+        prefix[i + 1] = prefix[i] + nums[i];
     }
+
+    long long total = prefix[n];
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == 0) {
+            long long left = prefix[i];
+            long long right = total - prefix[i + 1];
+
+            if (left == right) {
+                count += 2;
+            } else if (abs(left - right) == 1) {
+                count += 1;
+            }
+        }
+    }
+
+    return count;
+}
 };
