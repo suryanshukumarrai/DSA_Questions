4fcf6e1a13fd380c97aea3c73cacf2be4f983958
SURYANSHU KUMAR RAI <117182159+suryanshukumarrai@users.noreply.github.com>
2025-10-31 12:19:41 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (99.54%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int>a;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                a.push_back(nums[i]);
+            }
+        }
+        return a;
+    }
+};
