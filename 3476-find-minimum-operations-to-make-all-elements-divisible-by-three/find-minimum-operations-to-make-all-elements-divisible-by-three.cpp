d12ce2196ee510489d11052a3aa9f4f96fdbbc5f
SURYANSHU KUMAR RAI <117182159+suryanshukumarrai@users.noreply.github.com>
2025-11-22 12:23:05 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (45.48%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -4,10 +4,10 @@ public:
         int temp=0;
         for(int i=0;i< nums.size();i++){
             int a=nums[i]-3;
-            if(nums[i]-3!=0){
-                if(a%3!=0){
+            if(nums[i]-3!=0 && a%3!=0){
+                
                     temp++;
-                }
+                
                 
             }
         }
