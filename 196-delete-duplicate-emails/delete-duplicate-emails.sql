33bdd329a5d314deda11aae6706c5cf90ec7336c
SURYANSHU KUMAR RAI <117182159+suryanshukumarrai@users.noreply.github.com>
2025-12-18 16:50:27 +0530

    Time: 779 ms (71.26%) | Memory: 0B (100.00%) - LeetSync

---
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+
+DELETE 
+    p1
+FROM
+    person as p1
+INNER JOIN
+    person as p2
+ON
+    p1.email=p2.email
+    AND
+    p1.id>p2.id
